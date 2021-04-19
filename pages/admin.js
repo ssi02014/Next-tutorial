1e8e39390faa8dac51776ad2a4e9db364cdbc4cd
JeonMinJae <ssi02014@naver.com>
2021-04-20 01:32:39 +0900

    Remove(minjae): next-tutorial-scss-redux folder Remove

--- pages/admin.js
+++ pages/admin.js
@@ -3,20 +3,6 @@ import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 
 const Admin = () => {
-  const router = useRouter();
-
-  const checkLogin = () => {
-    axios.get("/api/isLogin").then((res) => {
-      if (res.status === 200 && res.data.name) {
-      } else {
-        router.push("/login");
-      }
-    });
-  };
-
-  useEffect(() => {
-    checkLogin();
-  }, []);
   return <div>admin</div>;
 };
 
